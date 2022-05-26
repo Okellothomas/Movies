fbad1008cec87c19492da064729a2d68d4374358
Okellothomas <thomasolal33@gmail.com>
2022-05-27 00:43:57 +0300

    add the code to splash the activity from main to mainscreen

--- app/src/main/java/com/pro/moviescollection/MainActivity.java
+++ app/src/main/java/com/pro/moviescollection/MainActivity.java
@@ -2,7 +2,9 @@ package com.pro.moviescollection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // call the handler method and assign a handler variable to new handler();
+        Handler handler = new Handler();
+
+        // use the postdelayed method to splash the activity from main to mainscreen.
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainScreen.class);
+                startActivity(intent);
+                finish();
+            }
+            // set the time for the splash.
+        }, 3000);
     }
 }
