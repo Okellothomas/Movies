f74b1936321914bfafe5265e73cc7031d6e86071
Okellothomas <thomasolal33@gmail.com>
2022-05-29 12:59:23 +0300

    add the framgment for changing the background

---
+++ app/src/main/java/com/pro/moviescollection/BackgroundFragment.java
@@ -0,0 +1,18 @@
+package com.pro.moviescollection;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+public class BackgroundFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_background_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+        return rootView;
+    }
+}
